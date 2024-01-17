9dc18c73a1d0d842a341aca1cbfe151ec63cb8e3
Ghaith <ghaith990000@gmail.com>
2024-01-17 18:29:36 +0300

    add list filteration to know the total correct answers

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -25,6 +25,11 @@ class ResultsScreen extends StatelessWidget{
   }
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data){
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -34,7 +39,7 @@ class ResultsScreen extends StatelessWidget{
           children: [
             const Text('You answered X out of Y questions correctly!'),
             const SizedBox(height: 30,),
-            QuestionsSummary(getSummaryData()),
+            QuestionsSummary(summaryData),
             const SizedBox(height: 30,),
             TextButton(onPressed: (){}, child: const Text('Restart Quiz!'))
           ]
