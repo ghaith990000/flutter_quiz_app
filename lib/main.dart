0f04693bee1e331a21d3b087b2db630b8baee908
Ghaith <ghaith990000@gmail.com>
2024-01-17 12:57:11 +0300

    initialize flutter project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_application/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(child: StartScreen()),
+      ),
+    ),
+  );
+}
