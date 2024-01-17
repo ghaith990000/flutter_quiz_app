0bb6828efbe2e8be4854c943fa17f91a1eea7aa0
Ghaith <ghaith990000@gmail.com>
2024-01-17 13:25:20 +0300

    added button to the start_screen page

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,19 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(child: StartScreen()),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
